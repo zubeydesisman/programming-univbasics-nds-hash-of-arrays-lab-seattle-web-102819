3bb2f71c45b36f724304ad3352dae0624261def1
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-10-14 04:42:22 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -1,4 +1,4 @@
-require "pry"
+
 BASE_HOA = {
   :chipmunks => ["Alvin", "Simon", "Theodore"],
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
@@ -10,6 +10,6 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
   BASE_HOA[:show] << "name"
-  binding.pry
+  
 return BASE_HOA
 end
